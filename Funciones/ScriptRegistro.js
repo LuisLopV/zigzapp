3a333f226b1bcc3439769ee16a24e8b4408940c9
LuisLopV <144469144+LuisLopV@users.noreply.github.com>
2024-04-25 15:38:20 -0500

    Add files via upload

---
+++ Funciones/ScriptRegistro.js
@@ -0,0 +1,7 @@
+document.getElementById('registrationForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  // Validation logic goes here
+  // You can add custom validation code using JavaScript
+  // For example: Checking if required fields are filled, validating email format, etc.
+  // If validation passes, you can submit the form
+});
